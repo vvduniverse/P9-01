0f38ce650a918e3b7602f7d3a995e6d44090a33c
Vahtee Boo <vahteeboo@gmail.com>
2022-01-15 20:38:00 +0200

    Done

--- P9-01/ContentView.swift
+++ P9-01/ContentView.swift
@@ -9,8 +9,15 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Path { path in
+            path.move(to: CGPoint(x: 200, y: 100))
+            path.addLine(to: CGPoint(x: 100, y: 300))
+            path.addLine(to: CGPoint(x: 300, y: 300))
+            path.addLine(to: CGPoint(x: 200, y: 100))
+//            path.closeSubpath()
+        }
+//        .stroke(.blue, lineWidth: 10)
+        .stroke(.blue, style: StrokeStyle(lineWidth: 10, lineCap: .round, lineJoin: .round))
     }
 }
 
